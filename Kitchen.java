37fd80e09629a3ad1399acda5753db91420319c6
Kee-ma <kirgarlia@gmail.com>
2026-06-02 11:01:52 -0500

    Update Kitchen.java

--- Kitchen.java
+++ Kitchen.java
@@ -1,1 +1,5 @@
-
+//List of pizzas the chef can make 
+//(choose the crust, sauce, toppings (can choose multiple toppings))
+//"Wrong pizza! Try again (max 3 tries)"
+//"Pizza created putting in oven... (in oven for 1 min)
+//Get the pizza out of the oven! 10..9..8 seconds until it burns
