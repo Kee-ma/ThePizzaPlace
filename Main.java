c7bcdf96a673932949bc47a53d439270daa49f02
Kee-ma <kirgarlia@gmail.com>
2026-06-04 11:41:10 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,18 +2,27 @@ import java.util.scanner;
 
 public class Main {
   public static void main(String[] args) {
+    public static final String RESET = "\u001B[0m";
+    public static final String RED = "\u001B[31m";
+    public static final String GREEN = "\u001B[32m";
+    public static final String YELLOW = "\u001B[33m";
     Scanner scanner = new Scanner(System.in);
-    System.out.println("Ene")
 
     String input = scanner.next();
     if (input.equalsIgnoreCase("c")) {
-    System.out.println("");
+      System.out.println(YELLOW + "________________________" + RESET);
+      System.out.println(YELLOW + "Customers" + RESET);
+      System.out.println(YELLOW + "________________________" + RESET);
       }
     else if (input.equalsIgnoreCase("k")) {
-    System.out.println("");
+      System.out.println(RED + "___________________" + RESET);
+      System.out.println(RED + "Ingredient" + RESET);
+      System.out.println(RED + "___________________" + RESET);
       }
     else if (input.equalsIgnoreCase("r")) {
-    System.out.println("");
+      System.out.println(GREEN + "___________________" + RESET);
+      System.out.println(GREEN + "Truck" + RESET);
+      System.out.println(GREEN + "___________________" + RESET);
       }
     else {
     System.out.println("Not a valid option")
