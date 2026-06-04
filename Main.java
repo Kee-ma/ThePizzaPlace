741921732910f5a6a07e09865734c8be80e25055
Kee-ma <kirgarlia@gmail.com>
2026-06-04 11:23:20 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,1 +1,22 @@
+import java.util.scanner;
 
+public class Main {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("Ene")
+
+    String input = scanner.next();
+    if (input.equalsIgnoreCase("c")) {
+    System.out.println("");
+      }
+    else if (input.equalsIgnoreCase("k")) {
+    System.out.println("");
+      }
+    else if (input.equalsIgnoreCase("r")) {
+    System.out.println("");
+      }
+    else {
+    System.out.println("Not a valid option")
+      }
+  }
+}
