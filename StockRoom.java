8fa8166e10b5d33a48bbe12cae7eecd287eacaa2
Kee-ma <kirgarlia@gmail.com>
2026-06-09 10:18:59 -0500

    Update StockRoom.java

--- StockRoom.java
+++ StockRoom.java
@@ -1,2 +1,10 @@
 //The restock truck is here!
 //Math mini game to get suplies (gets harder every new delivery)
+public class StockRoom {
+
+  private long timeRestart;
+  private static final long truckArrivalTime = 10000;
+
+  if (System.currentTimeMillis() >= truckArrivalTime + timeRestart && input.equalsIgnoreCase("l");
+
+}
