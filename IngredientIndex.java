cf07a21872a26e270b945b9c5c3734c37a2ba863
Kee-ma <kirgarlia@gmail.com>
2026-06-09 10:10:41 -0500

    Create IngredientIndex.java

---
+++ IngredientIndex.java
@@ -0,0 +1,14 @@
+public class IngredientIndex {
+  public static int small = 1;
+  public static int medium = 2;
+  public static int large = 3;
+
+  public static int normal = 1;
+  public static int thin = 2;
+  public static int stuffed = 3;
+
+  public static int tomatoes = 1;
+  public static int olives = 2;
+  public static int pepperoni = 3;
+  public static int pineapple = 4;
+}
