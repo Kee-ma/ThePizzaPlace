b06ee5d67af2cd09f86f6582a7c38a43a40e4225
Kee-ma <kirgarlia@gmail.com>
2026-06-09 01:48:01 -0500

    Update and rename Pizza.java to PizzaElements.java

---
+++ PizzaElements.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+
+public class PizzaElements {
+  private final int size;
+  private final int crust;
+  private final int sauce;
+  private List<String> toppings;
+
+  public class PizzaElements(int size, int crust, int  sauce, List<Integer> toppings) {
+    this.size = size;
+    this.crust = crust;
+    this.sauce = sauce;
+    this.toppings = new ArrayList<>(toppings);
+  }
+  
+  public int getSize {
+    return size;
+  }
+  public int getCrust {
+    return crust;
+  }
+  public int getSauce {
+    return sauce;
+  }
+  
+  public List<Integer> getToppings() {
+    return new ArrayList<>(toppings);
+  }
+}
