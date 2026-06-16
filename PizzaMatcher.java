55c765562128acc54cd0f4e289975bc43262d4fc
Kee-ma <kirgarlia@gmail.com>
2026-06-15 21:29:53 -0500

    Update PizzaMatcher.java
    DONE

--- PizzaMatcher.java
+++ PizzaMatcher.java
@@ -1,48 +1,60 @@
-import java.util.List;
 import java.util.ArrayList;
+import java.util.List;
 
 public class PizzaMatcher {
 
-        public static bolean matches(PizzaInfo orderInfo, PizzaInfo pizzaInfo) {
-            if(orderInfo.getSize() != pizzaInfo.getSize())
-                return false;
-            if(orderInfo.getCrust() != pizzaInfo.getCrust())
-                return false;
-            if(orderInfo.getSauce() != pizzaInfo.getSauce())
-                return false;
-    
-            List<Integer> orderToppings = new ArrayList<>(orderInfo.getToppings);
-            List<Integer> pizzaToppings = new ArrayList<>(pizzaInfo.getToppings);
-    
-            sort(orderToppings);
-            sort(pizzaToppings);
-    
-            return listEqual(orderToppings, pizzaToppings);
-        }
-    
-        private static void sort(List<Integer> lists) {
-            for (int i = 1; i < lists.size(); i++) {
-    
-                int value = lists.get(i);
-                int j = i - 1;
-    
-                while (j >= 0 && lists.get(j) > value) {
-                    lists.set(j + 1, lists.get(j));
-                    j -= 1;
-                }
-                lists.set(j + 1, value);
+    public static boolean matches(PizzaInfo orderInfo, PizzaInfo pizzaInfo) {
+
+        if (orderInfo.getSize() != pizzaInfo.getSize())
+            return false;
+
+        if (orderInfo.getCrust() != pizzaInfo.getCrust())
+            return false;
+
+        if (orderInfo.getSauce() != pizzaInfo.getSauce())
+            return false;
+
+        List<Integer> orderToppings = new ArrayList<>(orderInfo.getToppings());
+        List<Integer> pizzaToppings = new ArrayList<>(pizzaInfo.getToppings());
+
+        sort(orderToppings);
+        sort(pizzaToppings);
+
+        return sameList(orderToppings, pizzaToppings);
+    }
+
+    //Insertion sort so that ingredient order in our pizza and the customer's pizza match
+    //when we are comparing them
+    private static void sort(List<Integer> list) {
+
+        for (int i = 1; i < list.size(); i++) {
+
+            int value = list.get(i);
+            int j = i - 1;
+
+            while (j >= 0 && list.get(j) > value) {
+                list.set(j + 1, list.get(j));
+                j--;
             }
+
+            list.set(j + 1, value);
         }
-        private static boolean sameList(List<Integer> a, List<Integer> b) {
-    
-            if (a.size() != b.size())
+    }
+
+    //Compares just the ingredients- the method that compares the entire pizza can be found in the
+    //PizzaInfo class
+    private static boolean sameList(List<Integer> first, List<Integer> second) {
+
+        if (first.size() != second.size())
+            return false;
+
+        for (int i = 0; i < first.size(); i++) {
+
+            if (!first.get(i).equals(second.get(i)))
                 return false;
-    
-            for (int i = 0; i < a.size(); i++) {
-                if (a.get(i) != b.get(i)) {
-                    return false;
-            }
         }
-            return true;
-     }       
+
+        return true;
+    }
+
 }
