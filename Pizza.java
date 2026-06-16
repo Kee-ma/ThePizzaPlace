8c748524e6f7f080c178116e136236917ce2176f
Kee-ma <kirgarlia@gmail.com>
2026-06-15 21:31:24 -0500

    Update Pizza.java
    DONE

--- Pizza.java
+++ Pizza.java
@@ -1,1 +1,64 @@
+public class Pizza extends GameObject {
 
+    private PizzaInfo info;
+
+    private long ovenStartTime;
+    private long totalCookTime;
+
+    private boolean inOven;
+    private boolean removed;
+
+    public Pizza(PizzaInfo info) {
+        this.info = info;
+    }
+
+    public void putInOven() {
+        if (!inOven && !removed) {
+            inOven = true;
+            ovenStartTime = System.currentTimeMillis();
+        }
+    }
+
+    public void removeFromOven() {
+        if (inOven) {
+            totalCookTime =
+                    (System.currentTimeMillis() - ovenStartTime) / 1000;
+
+            inOven = false;
+            removed = true;
+        }
+    }
+
+    public long getTimeInOven() {
+        if (inOven) {
+            return (System.currentTimeMillis() - ovenStartTime) / 1000;
+        }
+
+        return totalCookTime;
+    }
+
+    public boolean isReady() {
+        return getTimeInOven() >= 60;
+    }
+
+    public boolean isBurned() {
+        return getTimeInOven() >= 70;
+    }
+
+    public boolean isInOven() {
+        return inOven;
+    }
+
+    public boolean isRemoved() {
+        return removed;
+    }
+
+    public PizzaInfo getInfo() {
+        return info;
+    }
+
+    //Ai fix
+    @Override
+    public void update() {
+    }
+}
