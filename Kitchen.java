f5d9ee00af20faf0ca2f29fab90122ac65825844
Kee-ma <kirgarlia@gmail.com>
2026-06-15 21:39:02 -0500

    Delete Kitchen.java

--- Kitchen.java
+++
@@ -1,7 +0,0 @@
-//List of pizzas the chef can make 
-//(choose the crust, sauce, toppings (can choose multiple toppings))
-//"Wrong pizza! Try again (max 3 tries)"
-//"Pizza created putting in oven... (in oven for 1 min)
-//Get the pizza out of the oven! 10..9..8 seconds until it burns
-public class Kitchen extends Room {
-}
