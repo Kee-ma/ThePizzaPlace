a2e1d3f17980bd93165fd0071335a9e0ad1cafa7
Kee-ma <kirgarlia@gmail.com>
2026-06-15 21:29:20 -0500

    Create IngredientCode.java
    DONE

---
+++ IngredientCode.java
@@ -0,0 +1,25 @@
+public class IngredientCode {
+
+    //Pizza size
+    public static final int SMALL = 1;
+    public static final int MEDIUM = 2;
+    public static final int LARGE = 3;
+
+    //Crust type
+    public static final int THIN = 1;
+    public static final int NORMAL = 2;
+    public static final int THICK = 3;
+
+    // Sauces
+    public static final int TOMATO = 1;
+    public static final int BBQ = 2;
+    public static final int ALFREDO = 3;
+
+    //Toppings
+    public static final int CHEESE = 1;
+    public static final int PEPPERONI = 2;
+    public static final int MUSHROOM = 3;
+    public static final int PINEAPPLE = 4;
+    public static final int ONION = 5;
+
+}
