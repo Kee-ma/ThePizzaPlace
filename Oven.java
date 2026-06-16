39d0ea6e23053e0b8e8c64b8e9ecc3973c079b77
Kee-ma <kirgarlia@gmail.com>
2026-06-15 21:26:03 -0500

    Update Oven.java
    DONE

--- Oven.java
+++ Oven.java
@@ -1,40 +1,56 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Oven {
-    private final int orderId;
-    private final PizzaInfo info;
-    
-    private boolean inOven = false;
-    private boolean burned = false;
-    private boolean completed = false;
-    
-    private long ovenStart;
-    private static final long bakeTime = 10000;
-    
-    public Pizza(int orderId, PizzaInfo info) {
-        this.orderId = orderId;
-        this.info = info;
+
+    private List<Pizza> pizzas;
+
+    public Oven() {
+        pizzas = new ArrayList<>();
     }
 
-    @Override
-    public void updateOven() {
-        if (inOven && !burned) {
-            if (System.currentTimeMillis() > ovenStart + bakeTime) {
-                burned = true;
-            }
+    public void addPizza(Pizza pizza) {
+
+        pizza.putInOven();
+        pizzas.add(pizza);
+    }
+
+    public List<Pizza> getPizzas() {
+        return pizzas;
+    }
+
+    public void showPizzas() {
+
+        if (pizzas.size() == 0) {
+            System.out.println("Oven is empty.");
+            return;
+        }
+
+        for (int i = 0; i < pizzas.size(); i++) {
+
+            Pizza pizza = pizzas.get(i);
+
+            System.out.println((i + 1) + ". Time in oven: " + pizza.getTimeInOven() + " seconds");
         }
     }
-    public void putInOven() {
-        inOven = true;
-        ovenStart = System.currentTimeMillis();
-    }
-    public boolean isBurned() {
-        return burned;
+
+    public Pizza removePizza(int index) {
+
+        if (index < 0 || index >= pizzas.size()) {
+            return null;
+        }
+
+        return pizzas.remove(index);
     }
 
-    public PizzaInfo getInfo() {
-        return info;
-    }
+    public boolean hasBurnedPizza() {
 
-    public int getOrderId() {
-        return orderId;
+        for (Pizza pizza : pizzas) {
+            if (pizza.isBurned()) {
+                return true;
+            }
+        }
+
+        return false;
     }
 }
