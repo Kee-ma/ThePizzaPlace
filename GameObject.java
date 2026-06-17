6bf64ef5c47e5a8c0dd358783d6a029ee8e8417f
Kee-ma <kirgarlia@gmail.com>
2026-06-16 19:32:07 -0500

    Update GameObject.java

--- GameObject.java
+++ GameObject.java
@@ -1,3 +1,5 @@
 public abstract class GameObject {
     public abstract void update();
 }
+//Code segment: Use of an abstract superclass as the parents of Pizza and Order. Pizza class' update method
+//updates the oven timer, while the update method in the Order class updates the orders.
